e3072b9c0b374baa0b8a7654dc86260ae7998bf6
“Gaai-san” <iwateangler@gmail.com>
2022-12-21 19:24:08 +0900

    fixed the button

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -18,12 +18,16 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from portfolioapp import views
+from django.views.static import serve
+
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.home, name='home'),
     path('blog/', include('blogapp.urls')),
+
+    
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
